68dbca56bc5cea2f9caed1236c8a7a387a48ba3f
coldperformer <coldperformer@gmail.com>
2021-06-30 15:33:59 +0530

    Mysql testn5

--- featureinstaller.sh
+++ featureinstaller.sh
@@ -9,11 +9,21 @@ sudo apt-get upgrade -y
 # Install Python pip
 sudo apt install python3-pip==21.1.1
 
+echo "----------PIP Installed----------"
+
+sudo apt-get install expect -y
+
+echo "----------expect Installed----------"
+
 # Required for Pycario and further for gcp
 sudo apt-get install libcairo2-dev
 
+echo "----------Libcario Installed----------"
+
 # Required for mysql connector and mysql and mysql client
 sudo apt-get install libmysqlclient-dev
+
+echo "----------MySQL Client Installed----------"
 
 # Install python packages
 pip3 install -r requirements.txt
@@ -39,7 +49,12 @@ sudo /etc/init.d/mysql start -u root -p[]
 
 echo "----------MySQL Server Started----------"
 
-# First time setup for secure installation
-sudo yes | mysql_secure_installation -u root -p 
+sudo mysql -u root -p -e "UPDATE mysql.user SET Password=PASSWORD('root') WHERE User='root';"
 
-echo "----------MySQL Installation Success!----------"
+echo "----------MySQL Client User Updated----------"
+
+# First time setup for secure installation
+# The MySQL secure installation is automated using expect
+# sudo yes | mysql_secure_installation -u root -p 
+
+# echo "----------MySQL Installation Success!----------"
